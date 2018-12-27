c0336b2c3c98e8b43f7748ffe6303e10dbeb4aec
Brian Marquis <rbmarquis.dev@gmail.com>
2018-12-27 17:30:21 -0600

    Use Promise.all to download json files and save into a single data object

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const dataUrl = "";
 
-const usEducationDataUrl = "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json";
-const usCountyDataUrl = "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json"; 
-let json, req = new XMLHttpRequest();
+// let json, req = new XMLHttpRequest();
 
 /** Send http req */
-req.open("GET", dataUrl ,true);
-req.send();
-req.onload = function() {
-  json = JSON.parse(req.responseText);
-  d3.select("main div#svg-container").text(JSON.stringify(json));
-};
+// req.open("GET", dataUrl ,true);
+// req.send();
+// req.onload = function() {
+//   json = JSON.parse(req.responseText);
+//   d3.select("main div#svg-container").text(JSON.stringify(json));
+// };
+
+const usCountyData = "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json"; 
+
+const usEducationData = "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json";
+
+const dataUrls = [usCountyData, usEducationData];
+
+let data = {};
+
+Promise.all(dataUrls.map(url => d3.json(url)))
+  .then(function(values) {
+    data.geo = values[0];
+    data.edu = values[1];
+    drawSvg();
+  })
+;
+
+function drawSvg() {
+
+  console.log(data);
+
+  
+
+
+}
+
+
 
